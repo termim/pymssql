6b3d159ec5d653b118b599cbac92ddadcb3306d2
Mikhail Terekhov <termim@gmail.com>
2020-08-27 18:12:48 -0400

    echo

--- dev/install.ps1
+++ dev/install.ps1
@@ -1,13 +1,27 @@
 $sql_express_download_url = "https://go.microsoft.com/fwlink/?linkid=829176"
 
+Write-Host "Download $sql_express_download_url"
 Invoke-WebRequest -Uri $sql_express_download_url -OutFile sqlexpress.exe
+
+Write-Host "Start-Process -Wait -FilePath .\sqlexpress.exe -ArgumentList /qs, /x:setup"
 Start-Process -Wait -FilePath .\sqlexpress.exe -ArgumentList /qs, /x:setup
+
+Write-Host ".\setup\setup.exe /q /ACTION=Install /INSTANCENAME=SQLEXPRESS /FEATURES=SQLEngine /UPDATEENABLED=0 /SQLSVCACCOUNT='NT AUTHORITY\System' /SQLSYSADMINACCOUNTS='BUILTIN\ADMINISTRATORS' /TCPENABLED=1 /NPENABLED=0 /IACCEPTSQLSERVERLICENSETERMS"
 .\setup\setup.exe /q /ACTION=Install /INSTANCENAME=SQLEXPRESS /FEATURES=SQLEngine /UPDATEENABLED=0 /SQLSVCACCOUNT='NT AUTHORITY\System' /SQLSYSADMINACCOUNTS='BUILTIN\ADMINISTRATORS' /TCPENABLED=1 /NPENABLED=0 /IACCEPTSQLSERVERLICENSETERMS
+
+Write-Host "Remove-Item -Recurse -Force sqlexpress.exe, setup"
 Remove-Item -Recurse -Force sqlexpress.exe, setup
 
+Write-Host "stop-service MSSQL`$SQLEXPRESS"
 stop-service MSSQL`$SQLEXPRESS
+
+Write-Host "set-itemproperty -path 'HKLM:\software\microsoft\microsoft sql server\mssql14.SQLEXPRESS\mssqlserver\supersocketnetlib\tcp\ipall' -name tcpdynamicports -value ''"
 set-itemproperty -path 'HKLM:\software\microsoft\microsoft sql server\mssql14.SQLEXPRESS\mssqlserver\supersocketnetlib\tcp\ipall' -name tcpdynamicports -value ''
+
+Write-Host "set-itemproperty -path 'HKLM:\software\microsoft\microsoft sql server\mssql14.SQLEXPRESS\mssqlserver\supersocketnetlib\tcp\ipall' -name tcpport -value 1433"
 set-itemproperty -path 'HKLM:\software\microsoft\microsoft sql server\mssql14.SQLEXPRESS\mssqlserver\supersocketnetlib\tcp\ipall' -name tcpport -value 1433
+
+Write-Host "set-itemproperty -path 'HKLM:\software\microsoft\microsoft sql server\mssql14.SQLEXPRESS\mssqlserver\' -name LoginMode -value 2"
 set-itemproperty -path 'HKLM:\software\microsoft\microsoft sql server\mssql14.SQLEXPRESS\mssqlserver\' -name LoginMode -value 2
 
 #.\start -sa_password $env:sa_password -ACCEPT_EULA $env:ACCEPT_EULA -attach_dbs \"$env:attach_dbs\" -Verbose
